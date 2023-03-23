f675a45cc24f78efb83d400cd7a388af9e4aef96
willinho10 <97019724+willinho10@users.noreply.github.com>
2023-03-23 12:24:08 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# ProjetLanguage
+# Project Analysis of PGN files
+
+## Description
+
+The project is a simple analysis of PGN files. The PGN files are chess games. The analysis is done in Python. If the file is not a PGN file, the program will output where the error is.
+
+## Usage 
+
+To run the program, you need to have Python installed. 
+If you want to change the input data you have to put it as a string in the variable "your_string" at line 38. The input must be inline and be carreful to the spaces. 
--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-# Script to check if the input is a PGN file or not, by lexical analysis and grammar analysis
-
-import ply.lex as lex
-import ply.yacc as yacc
 import re
 
 
@@ -39,7 +35,6 @@ class Move:
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    your_string3 = '[Event "Rated Rapid game"] [Site "https://lichess.org/Pt4RCiMl"] [Date "2023.03.23"] [White "QueenOfPearls"] [Black "AidenSilverion"] [Result "0-1"] [UTCDate "2023.03.23"] [UTCTime "08:01:49"] [WhiteElo "1334"] [BlackElo "1395"] [WhiteRatingDiff "-124"] [BlackRatingDiff "+6"] [Variant "Standard"] [TimeControl "600+5"] [ECO "D05"] [Opening "Queen\'s Pawn Game: Colle System"] [Termination "Normal"] 1. d4 d5 2. e3 Nf6 3. Nf3 e6 4. Be2 Nc6 5. h3 Be7 6. O-O Bd7 7. Nc3 O-O 8. a4 Bd6 9. Qd3 e5 10. dxe5 Nxe5 11. Nxd5 Nxd3 12. Bxd3 Nxd5 0-1'
     your_string = '[Event "Mannheim"] [Site "Mannheim GER"] [Date "1914.08.01"] [EventDate "1914.07.20"] [Round "11"] ' \
                   '[Result "1-0"] [White "Alexander Alekhine"] [Black "Hans Fahrni"] [ECO "C13"] [WhiteElo "?"] [' \
                   'BlackElo "?"] [PlyCount "45"] 1. e4 {Notes by Richard Reti} 1... e6 2. d4 d5 3. Nc3 Nf6 4. Bg5 Be7 ' \
@@ -54,19 +49,6 @@ if __name__ == '__main__':
                   'a surprising move and one difficult to have foreseen, forms the kernel of an apparently simple ' \
                   'Alekhine combination.} 20... Qg7 {After 20.Qe7 21.Qa5+ b6 22.Qc3 would follow.} 21. Nd6 Nb6 22. ' \
                   'Ne8 Qf7 {White mates in three moves.} 23. Qd6+ 1-0 '
-
-    your_string1 = '1. e4 {hello its me} 1... d5 {Scandinavian defence (often follows 2. exd5 Da5)} '
-    your_string2 = '1. e4 {Notes by Richard Reti} 1... e6 2. d4 d5 3. Nc3 Nf6 4. Bg5 Be7 5. e5 Nfd7 ' \
-                   '6. h4 {This ingenious method of play which has subsequently been adopted by all modern masters is ' \
-                   'characteristic of Alekhine’s style.} 6... Bxg5 7. hxg5 Qxg5 8. Nh3 {! The short-stepping knight is ' \
-                   'always brought as near as possible to the actual battle field. Therefore White does not make the ' \
-                   'plausible move 8 Nf3 but 8 Nh3 so as to get the knight to f4.} 8... Qe7 9. Nf4 Nf8 10. Qg4 f5 {The ' \
-                   'only move. Not only was 11 Qxg7 threatened but also Nxd5.} 11. exf6 gxf6 12. O-O-O {He again threatens ' \
-                   'Nxd5.} 12... c6 13. Re1 Kd8 14. Rh6 e5 15. Qh4 Nbd7 16. Bd3 e4 17. Qg3 Qf7 {Forced - the sacrifice of ' \
-                   'the knight at d5 was threatened and after 17...Qd6 18 Bxe4 dxe4 19 Rxe4 and 20 Qg7 wins.} 18. Bxe4 ' \
-                   'dxe4 19. Nxe4 Rg8 20. Qa3 {Here, as so often happens, a surprising move and one difficult to have ' \
-                   'foreseen, forms the kernel of an apparently simple Alekhine combination.} 20... Qg7 {After 20.Qe7 ' \
-                   '21.Qa5+ b6 22.Qc3 would follow.} 21. Nd6 Nb6 22. Ne8 Qf7 {White mates in three moves.} 23. Qd6+ 1-0 '
 
     title_regex = r"\[[a-zA-Z\s]*\s\"[^\"]*\"\]\s*"
 
@@ -138,8 +120,6 @@ if __name__ == '__main__':
     t_CAPTURE_PAWN = r'\sP?[a-h][x]+[a-h][1-8]\+?'
     t_Castle = r'O-O-O|O-O'
 
-    moveVariable = Move(0, "", "")
-
     tabFinal = []
     if lastMove != "":
         idLastMove = int(re.search(id_regex, lastMove).group().replace(".", ""))
@@ -156,8 +136,6 @@ if __name__ == '__main__':
         if re.search(r"\{.*\}\s+", line):
             comment.append(re.search(r"\{.*\}\s+", line).group())
             line = line.replace(re.search(r"\{.*\}\s+", line).group(), "")
-
-        print(comment)
 
         line = line.replace(id, "")
         id = int(id.replace(".", ""))
