7ae858f35b1f72c7e4d971a670e8b9aa99e2cd32
willinho10 <97019724+willinho10@users.noreply.github.com>
2023-03-23 09:26:38 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -50,15 +50,32 @@ class Move:
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+
+    your_string3 = '[Event "Rated Rapid game"] [Site "https://lichess.org/Pt4RCiMl"] [Date "2023.03.23"] [White "QueenOfPearls"] [Black "AidenSilverion"] [Result "0-1"] [UTCDate "2023.03.23"] [UTCTime "08:01:49"] [WhiteElo "1334"] [BlackElo "1395"] [WhiteRatingDiff "-124"] [BlackRatingDiff "+6"] [Variant "Standard"] [TimeControl "600+5"] [ECO "D05"] [Opening "Queen\'s Pawn Game: Colle System"] [Termination "Normal"] 1. d4 d5 2. e3 Nf6 3. Nf3 e6 4. Be2 Nc6 5. h3 Be7 6. O-O Bd7 7. Nc3 O-O 8. a4 Bd6 9. Qd3 e5 10. dxe5 Nxe5 11. Nxd5 Nxd3 12. Bxd3 Nxd5 0-1'
+    your_string = '[Event "Mannheim"] [Site "Mannheim GER"] [Date "1914.08.01"] [EventDate "1914.07.20"] [Round "11"] ' \
+                  '[Result "1-0"] [White "Alexander Alekhine"] [Black "Hans Fahrni"] [ECO "C13"] [WhiteElo "?"] [' \
+                  'BlackElo "?"] [PlyCount "45"] 1. e4 {Notes by Richard Reti} 1... e6 2. d4 d5 3. Nc3 Nf6 4. Bg5 Be7 ' \
+                  '5. e5 Nfd7 6. h4 {This ingenious method of play which has subsequently been adopted by all modern ' \
+                  'masters is characteristic of Alekhine’s style.} 6... Bxg5 7. hxg5 Qxg5 8. Nh3 {! The ' \
+                  'short-stepping knight is always brought as near as possible to the actual battle field. Therefore ' \
+                  'White does not make the plausible move 8 Nf3 but 8 Nh3 so as to get the knight to f4.} 8... Qe7 9. ' \
+                  'Nf4 Nf8 10. Qg4 f5 {The only move. Not only was 11 Qxg7 threatened but also Nxd5.} 11. exf6 gxf6 ' \
+                  '12. O-O-O {He again threatens Nxd5.} 12... c6 13. Re1 Kd8 14. Rh6 e5 15. Qh4 Nbd7 16. Bd3 e4 17. ' \
+                  'Qg3 Qf7 {Forced - the sacrifice of the knight at d5 was threatened and after 17...Qd6 18 Bxe4 dxe4 ' \
+                  '19 Rxe4 and 20 Qg7 wins.} 18. Bxe4 dxe4 19. Nxe4 Rg8 20. Qa3 {Here, as so often happens, ' \
+                  'a surprising move and one difficult to have foreseen, forms the kernel of an apparently simple ' \
+                  'Alekhine combination.} 20... Qg7 {After 20.Qe7 21.Qa5+ b6 22.Qc3 would follow.} 21. Nd6 Nb6 22. ' \
+                  'Ne8 Qf7 {White mates in three moves.} 23. Qd6+ 1-0 '
+
     your_string1 = '1. e4 {hello its me} 1... d5 {Scandinavian defence (often follows 2. exd5 Da5)} '
-    your_string = '1. e4 {Notes by Richard Reti} 1... e6 2. d4 d5 3. Nc3 Nf6 4. Bg5 Be7 h3 5. e5 Nfd7 ' \
+    your_string2 = '1. e4 {Notes by Richard Reti} 1... e6 2. d4 d5 3. Nc3 Nf6 4. Bg5 Be7 5. e5 Nfd7 ' \
                   '6. h4 {This ingenious method of play which has subsequently been adopted by all modern masters is ' \
                   'characteristic of Alekhine’s style.} 6... Bxg5 7. hxg5 Qxg5 8. Nh3 {! The short-stepping knight is ' \
                   'always brought as near as possible to the actual battle field. Therefore White does not make the ' \
                   'plausible move 8 Nf3 but 8 Nh3 so as to get the knight to f4.} 8... Qe7 9. Nf4 Nf8 10. Qg4 f5 {The ' \
                   'only move. Not only was 11 Qxg7 threatened but also Nxd5.} 11. exf6 gxf6 12. O-O-O {He again threatens ' \
                   'Nxd5.} 12... c6 13. Re1 Kd8 14. Rh6 e5 15. Qh4 Nbd7 16. Bd3 e4 17. Qg3 Qf7 {Forced - the sacrifice of ' \
-                  'the knight at d5 was threatened and after 17...Qd6 18 Bxe4 dxe4 19 Rxe4 and 20 Qg7 wins.} 19. Bxe4 ' \
+                  'the knight at d5 was threatened and after 17...Qd6 18 Bxe4 dxe4 19 Rxe4 and 20 Qg7 wins.} 18. Bxe4 ' \
                   'dxe4 19. Nxe4 Rg8 20. Qa3 {Here, as so often happens, a surprising move and one difficult to have ' \
                   'foreseen, forms the kernel of an apparently simple Alekhine combination.} 20... Qg7 {After 20.Qe7 ' \
                   '21.Qa5+ b6 22.Qc3 would follow.} 21. Nd6 Nb6 22. Ne8 Qf7 {White mates in three moves.} 23. Qd6+ 1-0 '
@@ -87,7 +104,6 @@ if __name__ == '__main__':
     match = re.search(result, content)
 
     if match:
-        print("There is a correct result")
         win = match.group()
         # content = content without the result
 
@@ -129,7 +145,10 @@ if __name__ == '__main__':
     moveVariable = Move(0, "", "")
 
     tabFinal = []
-    idLastMove = int(re.search(id_regex, lastMove).group().replace(".", ""))
+    if lastMove:
+        idLastMove = int(re.search(id_regex, lastMove).group().replace(".", ""))
+    else:
+        idLastMove = 0
     for line in tabMoves:
         comment = []
         id = re.search(id_regex, line).group()
@@ -142,6 +161,7 @@ if __name__ == '__main__':
             comment.append(re.search(r"\{.*\}\s+", line).group())
             line = line.replace(re.search(r"\{.*\}\s+", line).group(), "")
 
+        line = line.replace(id, "")
         id = int(id.replace(".", ""))
         move = re.findall(t_MOVE, line)
         move_pawn = re.findall(t_MOVE_PAWN, line)
@@ -151,16 +171,24 @@ if __name__ == '__main__':
 
         moveGlobal = move + move_pawn + capture + capture_pawn + castle
 
+        if moveGlobal:
+            for deleteMove in moveGlobal:
+                line = line.replace(deleteMove, "")
+
+        print(line)
+
         if len(moveGlobal) != 2:
             if len(moveGlobal) == 1:
                 print("There is only one correct move")
-                print(id)
+                print(f"At move number {id}")
                 verify += 1
             elif len(moveGlobal) == 0:
                 print("There is no correct move")
+                print(f"At move number {id}")
                 verify += 1
             else:
                 print("There is more than 2 correct moves")
+                print(f"At move number {id}")
                 verify += 1
 
         tabFinal.append(Move(id, moveGlobal, comment))
@@ -174,30 +202,30 @@ if __name__ == '__main__':
             verify += 1
             print(f"The PGN file doesn't have increasing moves : Error at move {tabFinal[i].id}")
             break
+    if idLastMove != 0:
+        if idLastMove != tabFinal[len(tabFinal) - 1].id + 1:
+            print(f"The PGN file doesn't have increasing moves : Error at move {idLastMove}")
+            verify += 1
 
-    if idLastMove != tabFinal[len(tabFinal) - 1].id:
-        print(f"The PGN file doesn't have increasing moves : Error at move {idLastMove}")
-        verify += 1
+        l_MOVE = r'[RNBQK][a-hA-H]+[1-8]\+*'
+        l_MOVE_PAWN = r'\sP?[a-h]+[1-8]\+*'
+        l_CAPTURE = r'[RNBQK][x][a-hA-H]+[1-8]\+*'
+        l_CAPTURE_PAWN = r'\sP?[a-h][x]+[a-h][1-8]\+*'
 
-    l_MOVE = r'[RNBQK][a-hA-H]+[1-8]\+*'
-    l_MOVE_PAWN = r'\sP?[a-h]+[1-8]\+*'
-    l_CAPTURE = r'[RNBQK][x][a-hA-H]+[1-8]\+*'
-    l_CAPTURE_PAWN = r'\sP?[a-h][x]+[a-h][1-8]\+*'
+        latsMove_pawn = re.findall(l_MOVE_PAWN, lastMove)
+        lastCapture = re.findall(l_CAPTURE, lastMove)
+        lastCapture_pawn = re.findall(l_CAPTURE_PAWN, lastMove)
+        lastMove = re.findall(l_MOVE, lastMove)
 
-    latsMove_pawn = re.findall(l_MOVE_PAWN, lastMove)
-    lastCapture = re.findall(l_CAPTURE, lastMove)
-    lastCapture_pawn = re.findall(l_CAPTURE_PAWN, lastMove)
-    lastMove = re.findall(l_MOVE, lastMove)
+        moveGlobal = lastMove + latsMove_pawn + lastCapture + lastCapture_pawn
 
-    moveGlobal = lastMove + latsMove_pawn + lastCapture + lastCapture_pawn
+        if len(moveGlobal) > 2:
+            print("There is more than 2 correct moves")
+            verify += 1
 
-    if len(moveGlobal) > 2:
-        print("There is more than 2 correct moves")
-        verify += 1
-
-    if len(moveGlobal) == 0:
-        print("There is no correct move")
-        verify += 1
+        if len(moveGlobal) == 0:
+            print("There is no correct move")
+            verify += 1
 
     if verify != 0:
         print("The PGN file is not correct")
